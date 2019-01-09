d958feda420e770cb34014d47f050c48cd8bb5c7
fourside <fourside@gmail.com>
2019-01-09 21:36:01 +0900

    chore

--- feeder/config/unicorn.conf.rb
+++ feeder/config/unicorn.conf.rb
@@ -1,6 +1,6 @@
 
 worker_processes 2
-app_root = ENV["APP_ROOT"]
+app_root = ENV["APP_ROOT"] || File.dirname(__FILE__) + "/.."
 working_directory app_root
 
 listen "#{app_root}/logs/unicorn.sock", :backlog => 1
