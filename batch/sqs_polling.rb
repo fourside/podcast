3bf58bfbfccc951f50654220edea397a829782e8
fourside <fourside@gmail.com>
2020-10-27 17:43:53 +0900

    sqs polling script

---
+++ batch/sqs_polling.rb
@@ -0,0 +1,18 @@
+require 'aws-sdk'
+
+url = env['SQS_URL']
+
+sqs = Aws::SQS::Client.new(region: 'ap-northeast-1')
+
+resp = sqs.receive_message(queue_url: url, max_number_of_messages: 10, wait_time_seconds: 20)
+
+resp.messages.each do |m|
+  body = JSON.parse(m.body)
+  stationId = body["stationId"]
+  fromTime = body["fromTime"]
+  duration = body["duration"]
+  title = body["title"]
+  personality = body["personality"]
+  spawn "./rec_radiko_ts.sh -s #{stationId} -f #{fromTime} -d #{duration} -T #{title} -a #{personality}"
+  sqs.delete_message(queue_url: url, receipt_handle: m.receipt_handle)
+end
