6af5d1e6bfe469db8c4df8b861e867ca87f3e201
fourside <fourside@gmail.com>
2021-03-25 09:51:01 +0900

    bugfix: delete sqs message after completion

--- timefree/sqs_polling.rb
+++ timefree/sqs_polling.rb
@@ -13,7 +13,7 @@ def process_queue_message message
   fromTime = body["fromTime"]
   duration = body["duration"]
   if is_future(fromTime, duration)
-    return
+    return false
   end
   stationId = body["stationId"]
   title = Shellwords.escape(body["title"])
@@ -21,8 +21,8 @@ def process_queue_message message
   command = "./rec_radiko_ts.sh -s #{stationId} -f #{fromTime} -d #{duration} -T #{title} -a #{personality}"
   puts command
   spawend = spawn command
-  sqs.delete_message(queue_url: url, receipt_handle: m.receipt_handle)
   Process.wait spawend
+  return true
 end
 
 url = ENV['SQS_URL']
@@ -32,10 +32,14 @@ sqs = Aws::SQS::Client.new(region: 'ap-northeast-1')
 
 resp = sqs.receive_message(queue_url: url, max_number_of_messages: 10, wait_time_seconds: 20)
 resp.messages.each do |message|
-  process_queue_message message
+  if process_queue_message message
+    sqs.delete_message(queue_url: url, receipt_handle: message.receipt_handle)
+  end
 end
 
 dead_resp = sqs.receive_message(queue_url: dead_letter_url, max_number_of_messages: 10, wait_time_seconds: 20)
 dead_resp.messages.each do |message|
-  process_queue_message message
+  if process_queue_message message
+    sqs.delete_message(queue_url: dead_letter_url, receipt_handle: message.receipt_handle)
+  end
 end
