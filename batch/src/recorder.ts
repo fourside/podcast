30dd7a4ef4951fb44ff7e9ff5a307f5d3d336518
fourside <fourside@gmail.com>
2022-01-07 23:32:24 +0900

    run process

---
+++ batch/src/recorder.ts
@@ -0,0 +1,62 @@
+import { formatTimeForFfmpeg } from "./date.ts";
+import { RecRadikoError } from "./rec-radiko-error.ts";
+
+export type RecordMeta = {
+  station: string;
+  duration: number;
+  title: string;
+  artist: string;
+  year: number;
+  outputFileName: string;
+};
+
+export async function record(
+  recordMeta: RecordMeta,
+  authToken: string,
+  playListUrl: string,
+): Promise<void> {
+  const commands = [
+    "ffmpeg",
+    "-loglevel",
+    "error",
+    "-fflags",
+    "+discardcorrupt",
+    "-headers",
+    `X-Radiko-Authtoken: ${authToken}`,
+    "-i",
+    playListUrl,
+    "-b:a",
+    "128k",
+    "-y",
+    "-t",
+    formatTimeForFfmpeg(recordMeta.duration),
+    "-metadata",
+    `title=${recordMeta.title}`,
+    "-metadata",
+    `artist=${recordMeta.artist}`,
+    "-metadata",
+    `year=${recordMeta.year}`,
+    recordMeta.outputFileName,
+  ];
+  const { status, stdout, stderr } = await runPipedProcess(commands);
+  console.log("ffmpeg:", status);
+  if (status.success) {
+    console.log(new TextDecoder().decode(stdout));
+  } else {
+    const error = new TextDecoder().decode(stderr);
+    throw new RecRadikoError(error);
+  }
+}
+
+async function runPipedProcess(cmd: string[]): Promise<
+  { status: Deno.ProcessStatus; stdout: Uint8Array; stderr: Uint8Array }
+> {
+  const process = Deno.run({ cmd, stdout: "piped", stderr: "piped" });
+  const [status, stdout, stderr] = await Promise.all([
+    process.status(),
+    process.output(),
+    process.stderrOutput(),
+  ]);
+  process.close();
+  return { status, stdout, stderr };
+}
