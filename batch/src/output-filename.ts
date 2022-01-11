0da52664b4687da6e40ef2f8cc982ecfbcc1199a
fourside <fourside@gmail.com>
2022-01-12 08:56:17 +0900

    copy deno script

--- batch/src/output-filename.ts
+++
@@ -1,6 +0,0 @@
-import { format } from "std/datetime";
-
-export function getOutputFilename(title: string, date: Date): string {
-  const suffix = format(date, "yyyyMMdd");
-  return `${title}-${suffix}.mp3`;
-}
