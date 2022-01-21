2f4fbe2ce39f9c27b796cfff04180ca374877dd6
fourside <fourside@gmail.com>
2022-01-21 13:07:36 +0900

    basic auth at every route

--- feeder/src/app.ts
+++ feeder/src/app.ts
@@ -1,9 +1,11 @@
 import fastify from "fastify";
 import type { FastifyInstance, FastifyServerOptions } from "fastify";
 import { routes } from "./router";
+import { basicAuth } from "./basic-auth";
 
 export function buildApp(options?: FastifyServerOptions): FastifyInstance {
   const app = fastify(options);
   app.register(routes);
+  app.addHook("preHandler", basicAuth);
   return app;
 }
--- feeder/src/basic-auth.ts
+++ feeder/src/basic-auth.ts
@@ -1,17 +1,17 @@
-import type { FastifyRequest, FastifyReply } from "fastify";
+import type { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from "fastify";
 import { Env } from "./env";
 
-export async function basicAuth(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+export function basicAuth(request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction): void {
   const auth = request.headers.authorization;
   if (auth === undefined) {
     reply.status(401).header("WWW-Authenticate", "Basic realm='private podcast'").send();
-    return;
+    done();
   }
   if (auth !== undefined && auth.toLocaleLowerCase().startsWith("basic ")) {
     const [, base64] = auth.split(" ");
     const [username, password] = Buffer.from(base64, "base64").toString().split(":");
     if (username === Env.getAllowedUser() && password === Env.getAllowedPassword()) {
-      // done
+      done();
     } else {
       reply.status(401).send("Unauthorized");
     }
