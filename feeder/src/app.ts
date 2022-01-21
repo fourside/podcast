429a63fa0a997ae63df980f53619b8484a2f2d17
fourside <fourside@gmail.com>
2022-01-21 13:15:04 +0900

    helmet for fastify

--- feeder/src/app.ts
+++ feeder/src/app.ts
@@ -1,11 +1,13 @@
 import fastify from "fastify";
 import type { FastifyInstance, FastifyServerOptions } from "fastify";
+import helmet from "fastify-helmet";
 import { routes } from "./router";
 import { basicAuth } from "./basic-auth";
 
 export function buildApp(options?: FastifyServerOptions): FastifyInstance {
   const app = fastify(options);
-  app.register(routes);
+  app.register(helmet);
   app.addHook("preHandler", basicAuth);
+  app.register(routes);
   return app;
 }
