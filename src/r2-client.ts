9cd15c087c6e3c6ed0217cae7430c1d02a1994cb
fourside <fourside@gmail.com>
2023-12-18 23:43:20 +0900

    replace s3 client

--- src/r2-client.ts
+++ src/r2-client.ts
@@ -1,34 +1,29 @@
 import { basename } from "std/path";
-import { PutObjectCommand, S3 } from "x/aws_sdk/s3-client";
+import { ApiFactory } from "x/aws_api/client";
+import { S3 as alterS3 } from "x/aws_api/s3";
 import { Env } from "./env.ts";
 import { getLogger } from "./logger.ts";
-import { RecRadikoError } from "./rec-radiko-error.ts";
 
 export async function putMp3(filePath: string): Promise<void> {
   const logger = getLogger("common");
-  const client = new S3({
-    endpoint: `https://${Env.cloudflare.accountId}.r2.cloudflarestorage.com`,
+  const s3 = new ApiFactory({
+    fixedEndpoint:
+      `https://${Env.cloudflare.accountId}.r2.cloudflarestorage.com`,
     region: "auto",
     credentials: {
-      accessKeyId: Env.cloudflare.accessKeyId,
-      secretAccessKey: Env.cloudflare.secretAccessKey,
+      awsAccessKeyId: Env.cloudflare.accessKeyId,
+      awsSecretKey: Env.cloudflare.secretAccessKey,
     },
-  });
+  }).makeNew(alterS3);
+
   const file = await Deno.readFile(filePath);
   const fileName = basename(filePath);
 
   logger.debug("put to r2 start");
-  const output = await client.send(
-    new PutObjectCommand({
-      Bucket: Env.cloudflare.bucketName,
-      Key: fileName,
-      Body: file,
-      ContentType: "audio/mp3",
-    }),
-  );
-  const statusCode = output.$metadata.httpStatusCode;
-  if (statusCode !== undefined && statusCode >= 400) {
-    logger.error("Failed to put R2", output);
-    throw new RecRadikoError(`Failed to put R2: ${fileName}`);
-  }
+  await s3.putObject({
+    Bucket: Env.cloudflare.bucketName,
+    Key: fileName,
+    Body: file,
+    ContentType: "audio/mp3",
+  });
 }
