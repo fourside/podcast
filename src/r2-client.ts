3f92d528aab38b1d6ebecd85cbb8383de74b8e12
fourside <fourside@gmail.com>
2023-12-20 21:33:57 +0900

    use awc cli

--- src/r2-client.ts
+++ src/r2-client.ts
@@ -1,29 +1,36 @@
 import { basename } from "std/path";
-import { ApiFactory } from "x/aws_api/client";
-import { S3 as alterS3 } from "x/aws_api/s3";
 import { Env } from "./env.ts";
 import { getLogger } from "./logger.ts";
+import { RecRadikoError } from "./rec-radiko-error.ts";
 
 export async function putMp3(filePath: string): Promise<void> {
   const logger = getLogger("common");
-  const s3 = new ApiFactory({
-    fixedEndpoint:
-      `https://${Env.cloudflare.accountId}.r2.cloudflarestorage.com`,
-    region: "auto",
-    credentials: {
-      awsAccessKeyId: Env.cloudflare.accessKeyId,
-      awsSecretKey: Env.cloudflare.secretAccessKey,
-    },
-  }).makeNew(alterS3);
 
-  const file = await Deno.readFile(filePath);
   const fileName = basename(filePath);
-
-  logger.debug("put to r2 start");
-  await s3.putObject({
-    Bucket: Env.cloudflare.bucketName,
-    Key: fileName,
-    Body: file,
-    ContentType: "audio/mp3",
+  const command = new Deno.Command("aws", {
+    args: [
+      "s3",
+      "cp",
+      "--endpoint",
+      `https://${Env.cloudflare.accountId}.r2.cloudflarestorage.com`,
+      "--region",
+      "auto",
+      filePath,
+      `s3://${Env.cloudflare.bucketName}/${fileName}`,
+    ],
+    stdout: "piped",
+    stderr: "piped",
+    env: {
+      AWS_ACCESS_KEY_ID: Env.cloudflare.accessKeyId,
+      AWS_SECRET_ACCESS_KEY: Env.cloudflare.secretAccessKey,
+    },
   });
+
+  const { success, stdout, stderr } = await command.output();
+  if (success) {
+    logger.info("success to upload to r2", new TextDecoder().decode(stdout));
+  } else {
+    const error = new TextDecoder().decode(stderr);
+    throw new RecRadikoError(error);
+  }
 }
