1cca83afeac3de322b137e0dfba957ed9f4c00bd
fourside <fourside@gmail.com>
2023-12-21 23:50:27 +0900

    use aws_api for s3 upload

--- src/r2-client.ts
+++ src/r2-client.ts
@@ -1,36 +1,31 @@
 import { basename } from "std/path";
+import { ApiFactory } from "x/aws_api/client";
+import { S3 } from "x/aws_api/s3";
+import { managedUpload } from "x/aws_api/s3-upload";
 import { Env } from "./env.ts";
 import { getLogger } from "./logger.ts";
-import { RecRadikoError } from "./rec-radiko-error.ts";
 
 export async function putMp3(filePath: string): Promise<void> {
   const logger = getLogger("common");
 
-  const fileName = basename(filePath);
-  const command = new Deno.Command("aws", {
-    args: [
-      "s3",
-      "cp",
-      "--endpoint",
+  const s3 = new ApiFactory({
+    fixedEndpoint:
       `https://${Env.cloudflare.accountId}.r2.cloudflarestorage.com`,
-      "--region",
-      "auto",
-      filePath,
-      `s3://${Env.cloudflare.bucketName}/${fileName}`,
-    ],
-    stdout: "piped",
-    stderr: "piped",
-    env: {
-      AWS_ACCESS_KEY_ID: Env.cloudflare.accessKeyId,
-      AWS_SECRET_ACCESS_KEY: Env.cloudflare.secretAccessKey,
+    region: "auto",
+    credentials: {
+      awsAccessKeyId: Env.cloudflare.accessKeyId,
+      awsSecretKey: Env.cloudflare.secretAccessKey,
     },
+  }).makeNew(S3);
+
+  const fileName = basename(filePath);
+  const file = await Deno.open(filePath, { read: true });
+
+  await managedUpload(s3, {
+    Bucket: Env.cloudflare.bucketName,
+    Key: fileName,
+    Body: file.readable,
   });
 
-  const { success, stdout, stderr } = await command.output();
-  if (success) {
-    logger.info("success to upload to r2", new TextDecoder().decode(stdout));
-  } else {
-    const error = new TextDecoder().decode(stderr);
-    throw new RecRadikoError(error);
-  }
+  logger.info(`send to r2: ${filePath}`);
 }
